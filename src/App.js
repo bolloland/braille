0e0576ebd221bd7f9977f0de5e75d1bfcb44c52a
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-05-16 23:25:11 -0500

    add initial containers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
 import './App.css';
+import BrailleContainer from './components/BrailleContainer'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-    
+    <Navbar />
     </div>
   );
 }
