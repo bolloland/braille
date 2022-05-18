7b95c1bc5c26f24bc74f6176d5f251e7cceb8a73
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-05-17 21:37:33 -0500

    add redux components

--- src/index.js
+++ src/index.js
@@ -2,19 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router} from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import dotReducer from './reducers/dotReducer';
+import thunk from 'redux-thunk'
 
-
-
-
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(dotReducer, composeEnhancers(applyMiddleware(thunk)))
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>   
         <Router> 
           <App />
         </Router> 
+    </Provider> 
   </React.StrictMode>
 );
 
