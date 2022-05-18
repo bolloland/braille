0d93317b77172408c983a4e48fdb5e94f1743574
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-05-17 21:37:13 -0500

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
 import BrailleContainer from './components/BrailleContainer'
+import DotContainer from './components/DotContainer';
 import Navbar from './components/Navbar';
+import { useNavigate, Routes, Route } from 'react-router'
+import Reference from './components/Reference';
+import Encrypt from './components/Encrypt';
+
 
 function App() {
   return (
     <div className="App">
     <Navbar />
+    <Routes>
+      <Route exact path="/" element={<BrailleContainer />} />
+      <Route exact path="/reference" element={<Reference />} />
+      <Route exact path="/encrypt" element={<Encrypt />} />
+      <Route />
+    </Routes>
     </div>
   );
 }
