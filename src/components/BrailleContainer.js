5016d09b9d2a7cdf197d67bf1cc6557cc452f0dc
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-27 15:25:25 -0500

    separate functions for button 1 & 2

--- src/components/BrailleContainer.js
+++ src/components/BrailleContainer.js
@@ -19,31 +19,52 @@ const BrailleContainer = () => {
   const [dot6value, setdot6Value] = useState("false")
   const [dot6text, setdot6Text] = useState("black")
 
- 
+  
+  const switch1Color = (id) => {
 
-  const switchColor = () => {
-     console.log(this)
+     console.log(id, "button 1")
     if (dot1value !== true) {
       setdot1Value(true)
       setdot1Text("blue")
     } else {
       setdot1Value(false)
       setdot1Text("black")
-    }    
+    } 
   }
+    
+   const switch2Color = (id) => {
+      console.log(id, "button 2")
+      if (dot2value !== true) {
+        setdot2Value(true)
+        setdot2Text("blue")
+      } else {
+        setdot2Value(false)
+        setdot2Text("black")
+      }   
+    } 
+   
+    // if (dot3value !== true) {
+    //   setdot3Value(true)
+    //   setdot3Text("blue")
+    // } else {
+    //   setdot3Value(false)
+    //   setdot3Text("black")
+    // }   
+  
 
   return (
     <div className="braille-container">
-      <button className="dot" id="dot1" value={dot1value} onClick={switchColor}>{dot1text}</button>
-      <button className="dot" id="dot2" value={dot2value} onClick={switchColor}>{dot2text}</button>
-      <button className="dot" id="dot3" value={dot3value} onClick={switchColor}>{dot3text}</button>
-      <button className="dot" id="dot4" value={dot4value} onClick={switchColor}>{dot4text}</button>
-      <button className="dot" id="dot5" value={dot5value} onClick={switchColor}>{dot5text}</button>
-      <button className="dot" id="dot6" value={dot6value} onClick={switchColor}>{dot6text}</button>
+      <button className="dot" id="dot1" value={dot1value} onClick={()=>switch1Color("dot1")}>{dot1text}</button>
+      <button className="dot" id="dot2" value={dot2value} onClick={()=>switch2Color("dot2")}>{dot2text}</button>
+      <button className="dot" id="dot3" value={dot3value} onClick={()=>switch1Color("dot3")}>{dot3text}</button>
+      <button className="dot" id="dot4" value={dot4value} onClick={()=>switch2Color("dot4")}>{dot4text}</button>
+      <button className="dot" id="dot5" value={dot5value} onClick={()=>switch1Color("dot5")}>{dot5text}</button>
+      <button className="dot" id="dot6" value={dot6value} onClick={()=>switch2Color("dot6")}>{dot6text}</button>
     
     </div>
 
 )
+
 }
 
 
