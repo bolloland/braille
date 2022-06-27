60c17f20bee41f95eac53c036e8c8933b8f12d0b
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-27 12:57:03 -0500

    create letters seed

---
+++ src/braille-data/seed.js
@@ -0,0 +1,18 @@
+const letters = [
+    {id: 1, 
+        letter: A, 
+        pattern: [true, false, false, false, false, false]}, 
+    {id: 2,
+        letter: B, 
+        pattern: [true, false, true, false, false, false]}, 
+    {id: 3,
+        letter: C, 
+        pattern: [true, true, false, false, false, false]},
+    {id: 4,
+        letter: D, 
+        pattern: [true, true, true, false, false, false]}
+    ]
+
+
+
+export default letters
