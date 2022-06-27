e055aa7dc732dea33e18d5ff9c3487fa9b5030ef
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-27 12:57:34 -0500

    init button images

---
+++ src/images/images.js
@@ -0,0 +1,13 @@
+// import { Component } from 'react'
+// import './blackdot.PNG'
+// import './blackdot.PNG'
+
+// const images = {
+//     const blackdot = './blackdot.PNG'
+//     const bluedot = './bluedot.PNG'
+// }    
+
+
+
+
+// export default images
