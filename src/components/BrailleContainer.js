bdff665a7baaa14fdd8b272bbcb859ebbd1ad921
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-27 12:57:18 -0500

    init Braille Container

--- src/components/BrailleContainer.js
+++ src/components/BrailleContainer.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import '../styles/braille-container.css'
+import "../images/blackdot.PNG"
 
 const BrailleContainer = () => {
+
+  const switchColor = () => {
+
+  }
+
+
   return (
-    <div>BrailleContainer</div>
+    <div className="braille-container">
+      <button className="dot" id="dot1" onClick={switchColor}>FALSE</button>
+      <button className="dot">FALSE</button>
+      <button className="dot">FALSE</button>
+      <button className="dot">FALSE</button>
+      <button className="dot">FALSE</button>
+      <button className="dot">FALSE</button>
+    
+    </div>
+
+
+
   )
 }
 
