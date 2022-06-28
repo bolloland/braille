6bbbdd352315157fe5297a4952eacfab3b4ecb2c
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-28 16:22:29 -0500

    condense switchColor function

--- src/components/BrailleContainer.js
+++ src/components/BrailleContainer.js
@@ -2,70 +2,55 @@ import React from 'react'
 import '../styles/braille-container.css'
 import "../images/blackdot.PNG"
 import { useState } from 'react'
+import { DOT_DATA } from '../braille-data/constants'
+
 
 const BrailleContainer = () => {
-  //onClick the ID should switch, and then alter the ID in the code. 
+  // create initial state as DOTDATA
+    const [dots, setDots] = useState(DOT_DATA)
 
-  const [dot1value, setdot1Value] = useState("false")
-  const [dot1text, setdot1Text] = useState("black")
-  const [dot2value, setdot2Value] = useState("false")
-  const [dot2text, setdot2Text] = useState("black")
-  const [dot3value, setdot3Value] = useState("false")
-  const [dot3text, setdot3Text] = useState("black")
-  const [dot4value, setdot4Value] = useState("false")
-  const [dot4text, setdot4Text] = useState("black")
-  const [dot5value, setdot5Value] = useState("false")
-  const [dot5text, setdot5Text] = useState("black")
-  const [dot6value, setdot6Value] = useState("false")
-  const [dot6text, setdot6Text] = useState("black")
+    const switchColor = (dot) => {
+      // can't alter state, so make a copy (newDot) and pass that to setState
+      let newDots = [...dots];
+      let newDot;
 
-  
-  const switch1Color = (id) => {
+      if (dot.value !== true) {
+          newDot = {
+            name: dot.name,
+            value: true,
+            text: "blue",
+            image: ""
+            }
+        } else {
+          newDot = {
+            name: dot.name,
+            value: false,
+            text: "black",
+            image: ""
+          }
+        }
+        // find and replace clicked dot
+        const dotIndex = dots.indexOf(dot)
+        newDots.splice(dotIndex, 1, newDot)
+        setDots(newDots)
+    }
 
-     console.log(id, "button 1")
-    if (dot1value !== true) {
-      setdot1Value(true)
-      setdot1Text("blue")
-    } else {
-      setdot1Value(false)
-      setdot1Text("black")
-    } 
-  }
     
-   const switch2Color = (id) => {
-      console.log(id, "button 2")
-      if (dot2value !== true) {
-        setdot2Value(true)
-        setdot2Text("blue")
-      } else {
-        setdot2Value(false)
-        setdot2Text("black")
-      }   
-    } 
-   
-    // if (dot3value !== true) {
-    //   setdot3Value(true)
-    //   setdot3Text("blue")
-    // } else {
-    //   setdot3Value(false)
-    //   setdot3Text("black")
-    // }   
-  
-
-  return (
+    return (
     <div className="braille-container">
-      <button className="dot" id="dot1" value={dot1value} onClick={()=>switch1Color("dot1")}>{dot1text}</button>
-      <button className="dot" id="dot2" value={dot2value} onClick={()=>switch2Color("dot2")}>{dot2text}</button>
-      <button className="dot" id="dot3" value={dot3value} onClick={()=>switch1Color("dot3")}>{dot3text}</button>
-      <button className="dot" id="dot4" value={dot4value} onClick={()=>switch2Color("dot4")}>{dot4text}</button>
-      <button className="dot" id="dot5" value={dot5value} onClick={()=>switch1Color("dot5")}>{dot5text}</button>
-      <button className="dot" id="dot6" value={dot6value} onClick={()=>switch2Color("dot6")}>{dot6text}</button>
-    
+     {dots && dots.map((dot) => {
+        return ( 
+          <button 
+          key={dot.name} 
+          value={dot.value} 
+          image={dot.image}
+          onClick={switchColor(dot)}
+          >{dot.text}</button>
+          )
+      })}
     </div>
 
-)
-
+    )
 }
-
 
 export default BrailleContainer
