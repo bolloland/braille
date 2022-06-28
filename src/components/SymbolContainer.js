b0c3e9e615beeecad843a70b10014e6a3f2611ea
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-28 18:45:03 -0500

    init Symbol Container

---
+++ src/components/SymbolContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/square-box.css'
+
+const SymbolContainer = () => {
+  return (
+    <div className="square-box">
+        
+            <span>A</span>
+        
+    </div>
+  )
+}
+
+export default SymbolContainer
