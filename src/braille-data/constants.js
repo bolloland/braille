0feceac37fb950cdc24086bd436fab529ebb2410
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-28 16:21:17 -0500

    init state of dot data

---
+++ src/braille-data/constants.js
@@ -0,0 +1,38 @@
+export const DOT_DATA = [
+    {
+        name: "dot1",
+        value: false,
+        text: "black",
+        image: ""
+    },
+    {
+        name: "dot2",
+        value: false,
+        text: "black",
+        image: ""
+    },
+    {
+        name: "dot3",
+        value: false,
+        text: "black",
+        image: ""
+    },
+    {
+        name: "dot4",
+        value: false,
+        text: "black",
+        image: ""
+    },
+    {
+        name: "dot5",
+        value: false,
+        text: "black",
+        image: ""
+    },
+    {
+        name: "dot6",
+        value: false,
+        text: "black",
+        image: ""
+    }
+]
