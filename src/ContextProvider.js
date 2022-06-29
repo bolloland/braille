6ea4709b1bb0c7f657462782787abae5a7aa4b93
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-29 00:49:18 -0500

    include {values} in Context Provider

--- src/ContextProvider.js
+++ src/ContextProvider.js
@@ -1,4 +1,4 @@
-import React, {useState, createContext } from 'react'
+import React, { useState, createContext } from 'react'
 import { DOT_DATA } from './braille-data/constants'
 
     //context created OUTSIDE of Provider
@@ -8,12 +8,17 @@ export const MyContext = createContext({
     })
 
 const ContextProvider = (props) => {
-    // console.log(MyContext)
+    console.log(MyContext)
     const [dots, setDots] = useState(DOT_DATA)
+
+    const values = {
+        dots: dots,
+        setDots: setDots
+    }
 
     return (
         <div>
-            <MyContext.Provider>
+            <MyContext.Provider value={values}>
                 {props.children}
             </MyContext.Provider>
         </div>
