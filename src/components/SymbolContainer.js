a2b03394f779b2350d0f67fdb4fe6365434b6c93
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-29 14:17:20 -0500

    create renderLetter to access(?) correct symbol

--- src/components/SymbolContainer.js
+++ src/components/SymbolContainer.js
@@ -5,32 +5,31 @@ import { letters } from '../braille-data/seed'
 
 const SymbolContainer = () => {
     const context = useContext(MyContext)
-    const { dots } = context
+    const {dots} = context
     const enteredArray = dots.map((dot) => dot.value)
     console.log(enteredArray, "enteredArray")
     
-    for (let i=0; i < letters.length; i++) {
-           if (letters[i].pattern[0] === enteredArray[0]
-            && letters[i].pattern[1] === enteredArray[1]
-            && letters[i].pattern[2] === enteredArray[2]
-            && letters[i].pattern[3] === enteredArray[3]
-            && letters[i].pattern[4] === enteredArray[4]
-            && letters[i].pattern[5] === enteredArray[5]) {
+    const renderLetter = () => {
 
+        for (let i=0; i < letters.length; i++) {
+            if (letters[i].pattern.every((value, index) => value === enteredArray[index])) {
+                    return (
+                       letters[i].symbol
+                    )
+            } else { 
                 return (
-                    <div className="square-box">
-                    {letters[i].symbol}
-                    </div>
-                )           
-            } else {
-                return (
-                <div className="square-box">
-                ?
-                </div>
-                )
-            }
+                    "?"
+                    )
+                }
+        }
+
     }
 
+    return (
+        <div className="square-box">
+       {renderLetter}
+        </div>
+        )           
 }
 
 export default SymbolContainer
