bff1e1be357fbe1182670252274a6ac827126ba5
Ryan Bollettino <ryan.bollettino@gmail.com>
2022-06-29 00:50:55 -0500

    letter renamed symbol

--- src/braille-data/seed.js
+++ src/braille-data/seed.js
@@ -1,14 +1,14 @@
 export const letters = [
     {id: 1, 
-        letter: "A", 
+        symbol: "A", 
         pattern: [true, false, false, false, false, false]}, 
     {id: 2,
-        letter: "B", 
+        symbol: "B", 
         pattern: [true, false, true, false, false, false]}, 
     {id: 3,
-        letter: "C", 
+        symbol: "C", 
         pattern: [true, true, false, false, false, false]},
     {id: 4,
-        letter: "D", 
+        symbol: "D", 
         pattern: [true, true, true, false, false, false]}
     ]
